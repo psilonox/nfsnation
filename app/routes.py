1889d5558c17759c49245c82de0f2fcb8a47272b
Bryan Hernandez <pdnbryan1998@gmail.com>
2018-02-12 15:03:40 -0800

    aa

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,7 @@
+from flask import render_template
 from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-     user = {'username', 'bryan623'}
-     return '''
-     <html>
-        <head>
-            <title>Home - NFSNation</title>
-        </head>
-        <body>
-        </body
-    </html>
+     user = {'username': 'bryan623'}
+     return render_template('index.html', title='Home', user=user)
